5694bff90d78e25d71c342ecf2cdc21555a15669
salehalakhras <93873314+salehalakhras@users.noreply.github.com>
2024-09-29 11:21:56 +0300

    upload a built app

--- code-editor/src/Routes/Auth.tsx
+++ code-editor/src/Routes/Auth.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import  {useState, useEffect} from 'react'
 import Register from '../Components/auth/Register';
 import Login from '../Components/auth/Login';
 
--- code-editor/src/Routes/Landing.tsx
+++ code-editor/src/Routes/Landing.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from '../Components/landing/Header'
 import Hero from '../Components/landing/Hero'
 import Benefits from '../Components/landing/Benefits'
